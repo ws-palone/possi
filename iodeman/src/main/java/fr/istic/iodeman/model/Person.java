d181063dbada18edf7838686bfa10a9035945722
Corentin <corentin.clement.pro@gmail.com>
2015-01-12 17:45:09 +0100

    Création de la classe Personne

--- iodeman/src/main/java/fr/istic/iodeman/model/Person.java
+++ iodeman/src/main/java/fr/istic/iodeman/model/Person.java
@@ -1,5 +1,35 @@
 package fr.istic.iodeman.model;
 
 public class Person {
-
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private Role role;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	
 }
