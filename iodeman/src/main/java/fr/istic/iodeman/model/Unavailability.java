6852e45220e6631dca8c6fd8f08712f85abd2098
David <contact@davidmichel.fr>
2015-01-12 17:38:50 +0100

    add implementation of Unavailability

--- iodeman/src/main/java/fr/istic/iodeman/model/Unavailability.java
+++ iodeman/src/main/java/fr/istic/iodeman/model/Unavailability.java
@@ -2,4 +2,23 @@ package fr.istic.iodeman.model;
 
 public class Unavailability {
 
+	private Person person;
+	private TimeBox period;
+	
+	public Person getPerson() {
+		return person;
+	}
+	
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	
+	public TimeBox getPeriod() {
+		return period;
+	}
+	
+	public void setPeriod(TimeBox period) {
+		this.period = period;
+	}
+	
 }
