62501333a047b6b17bb90565eda72cf60685dbb9
Corentin <corentin.clement.pro@gmail.com>
2015-01-12 17:38:38 +0100

    Création d'OralDefense

--- iodeman/src/main/java/fr/istic/iodeman/model/OralDefense.java
+++ iodeman/src/main/java/fr/istic/iodeman/model/OralDefense.java
@@ -1,5 +1,39 @@
 package fr.istic.iodeman.model;
 
-public class OralDefense {
+import java.util.Collection;
+import java.util.Iterator;
 
+public class OralDefense {
+	private Participant composition;
+	private Room room;
+	private TimeBox timebox;
+	private Collection<Person> jury;
+	
+	
+	public Participant getComposition() {
+		return composition;
+	}
+	public void setComposition(Participant composition) {
+		this.composition = composition;
+	}
+	public Room getRoom() {
+		return room;
+	}
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+	public TimeBox getTimebox() {
+		return timebox;
+	}
+	public void setTimebox(TimeBox timebox) {
+		this.timebox = timebox;
+	}
+	public Iterator<Person> getJury() {
+		return jury.iterator();
+	}
+	public void setJury(Collection<Person> jury) {
+		this.jury = jury;
+	}
+	
+	
 }
