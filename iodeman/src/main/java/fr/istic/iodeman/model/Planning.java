da6be970dc8acf41a484e1e71f4f3c00cc3d74b8
Corentin <corentin.clement.pro@gmail.com>
2015-01-12 17:28:14 +0100

    Création du Planning

--- iodeman/src/main/java/fr/istic/iodeman/model/Planning.java
+++ iodeman/src/main/java/fr/istic/iodeman/model/Planning.java
@@ -1,5 +1,49 @@
 package fr.istic.iodeman.model;
 
 public class Planning {
-
+	
+	private TimeBox period;
+	private Integer oralDefenseDuration;
+	private Integer oralDefenseInterlude;
+	private TimeBox lunchBreak;
+	private TimeBox dayPeriod;
+	private Integer nbMaxOralDefensePerDay;
+	
+	public TimeBox getPeriod() {
+		return period;
+	}
+	public void setPeriod(TimeBox period) {
+		this.period = period;
+	}
+	public Integer getOralDefenseDuration() {
+		return oralDefenseDuration;
+	}
+	public void setOralDefenseDuration(Integer oralDefenseDuration) {
+		this.oralDefenseDuration = oralDefenseDuration;
+	}
+	public Integer getOralDefenseInterlude() {
+		return oralDefenseInterlude;
+	}
+	public void setOralDefenseInterlude(Integer oralDefenseInterlude) {
+		this.oralDefenseInterlude = oralDefenseInterlude;
+	}
+	public TimeBox getLunchBreak() {
+		return lunchBreak;
+	}
+	public void setLunchBreak(TimeBox lunchBreak) {
+		this.lunchBreak = lunchBreak;
+	}
+	public TimeBox getDayPeriod() {
+		return dayPeriod;
+	}
+	public void setDayPeriod(TimeBox dayPeriod) {
+		this.dayPeriod = dayPeriod;
+	}
+	public Integer getNbMaxOralDefensePerDay() {
+		return nbMaxOralDefensePerDay;
+	}
+	public void setNbMaxOralDefensePerDay(Integer nbMaxOralDefensePerDay) {
+		this.nbMaxOralDefensePerDay = nbMaxOralDefensePerDay;
+	}
+	
 }
