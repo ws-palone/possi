9882ad12821bb0ab8059efab1235e95e8f30345c
David <contact@davidmichel.fr>
2015-01-12 17:35:35 +0100

    add implementation of Participant & TimeBox

--- iodeman/src/main/java/fr/istic/iodeman/model/Participant.java
+++ iodeman/src/main/java/fr/istic/iodeman/model/Participant.java
@@ -1,5 +1,24 @@
 package fr.istic.iodeman.model;
 
 public class Participant {
+	
+	private Person student;
+	private Person followingTeacher;
+	
+	public Person getStudent() {
+		return student;
+	}
+	
+	public void setStudent(Person student) {
+		this.student = student;
+	}
+	
+	public Person getFollowingTeacher() {
+		return followingTeacher;
+	}
+	
+	public void setFollowingTeacher(Person followingTeacher) {
+		this.followingTeacher = followingTeacher;
+	}	
 
 }
--- iodeman/src/main/java/fr/istic/iodeman/model/TimeBox.java
+++ iodeman/src/main/java/fr/istic/iodeman/model/TimeBox.java
@@ -1,5 +1,23 @@
 package fr.istic.iodeman.model;
 
-public class TimeBox {
+import java.util.Date;
 
+public class TimeBox {
+	
+	private Date from;
+	private Date to;
+	
+	public Date getFrom() {
+		return from;
+	}
+	public void setFrom(Date from) {
+		this.from = from;
+	}
+	public Date getTo() {
+		return to;
+	}
+	public void setTo(Date to) {
+		this.to = to;
+	}
+	
 }
