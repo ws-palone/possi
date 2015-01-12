a0bd46eeadebde5323c206c09b404f11d56d12c8
Corentin <corentin.clement.pro@gmail.com>
2015-01-12 17:30:50 +0100

    Création de la classe priority

--- iodeman/src/main/java/fr/istic/iodeman/model/Priority.java
+++ iodeman/src/main/java/fr/istic/iodeman/model/Priority.java
@@ -1,5 +1,20 @@
 package fr.istic.iodeman.model;
 
 public class Priority {
-
+	private Role role;
+	private Integer weight;
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	public Integer getWeight() {
+		return weight;
+	}
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+	
+	
 }
