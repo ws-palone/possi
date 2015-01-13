41d37bcbec8857106c33554de501bdc9a067218a
Kyozatch <lecut.alexandre@gmail.com>
2015-01-13 13:04:32 +0100

    cas

--- iodeman/src/main/java/fr/istic/iodeman/TestController.java
+++ iodeman/src/main/java/fr/istic/iodeman/TestController.java
@@ -43,7 +43,7 @@ public class TestController {
 	@RequestMapping("/login")
 	public ServiceTicketValidator login(@RequestParam(value="ticket", defaultValue="") String ticket){
 		
-		 String serverName = "https://sso-cas.univ-rennes1.fr/validate";
+		 String serverName = "https://sso-cas.univ-rennes1.fr/serviceValidate";
 		 String user = null;
 		 String errorCode = null;
 		 String errorMessage = null;
