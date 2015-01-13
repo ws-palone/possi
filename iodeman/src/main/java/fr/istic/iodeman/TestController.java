cbf1e00279e5bcbd801be7970368de17db25edca
Kyozatch <lecut.alexandre@gmail.com>
2015-01-13 11:14:47 +0100

    test login cas

--- iodeman/src/main/java/fr/istic/iodeman/TestController.java
+++ iodeman/src/main/java/fr/istic/iodeman/TestController.java
@@ -1,5 +1,9 @@
 package fr.istic.iodeman;
 
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.SAXException;
+
+import edu.yale.its.tp.cas.client.ServiceTicketValidator;
 
 
 @RestController  
@@ -31,6 +38,50 @@ public class TestController {
 		Greeting g = new Greeting();
 		g.setName(name);
 		return g;
+	}
+	
+	@RequestMapping("/login")
+	public void login(@RequestParam(value="ticket", defaultValue="") String ticket){
+		
+		System.out.println(ticket);
+		
+		/*
+		 String serverName = "sso-cas.univ-rennes1.fr";
+		 String user = null;
+		 String errorCode = null;
+		 String errorMessage = null;
+		 String xmlResponse = null;
+		  
+		 ServiceTicketValidator sv = new ServiceTicketValidator();
+		  
+		 sv.setCasValidateUrl(serverName);
+		 sv.setService("http://iode-man.istic.univ-rennes1.fr:8080/idoeman/");
+		 sv.setServiceTicket(ticket);
+		  
+		 try {
+			sv.validate();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  
+		 xmlResponse = sv.getResponse();
+		  
+		 if(sv.isAuthenticationSuccesful()) {
+		     user = sv.getUser();
+		 } else {
+		     errorCode = sv.getErrorCode();
+		     errorMessage = sv.getErrorMessage();
+		 }
+		 */	
+		 
+		
 	}
 	
 }  
--- iodeman/src/test/java/fr/istic/iodeman/TestConnection.java
+++ iodeman/src/test/java/fr/istic/iodeman/TestConnection.java
@@ -2,13 +2,17 @@ package fr.istic.iodeman;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
-public class TestConnection {
+import edu.yale.its.tp.cas.client.ServiceTicketValidator;
+import edu.yale.its.tp.cas.client.filter.CASFilter;
 
+public class TestConnection {
+	
 	@Test
-	public void test() {
+	public void sendRequest(){
+		String serverName = "https://sso-cas.univ-rennes1.fr/login?service=http://iode-man.istic.univ-rennes1.fr:8080/iodeman/";
 		assert(true);
 	}
-
 }
