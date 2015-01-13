7e105434f1ee6b3f1bde2b6b066a70caa359022d
Corentin <corentin.clement.pro@gmail.com>
2015-01-13 14:03:56 +0100

    Création de stratégie pour l'export Excel

---
+++ iodeman/src/main/java/fr/istic/iodeman/strategy/PlanningExcelExport.java
@@ -0,0 +1,15 @@
+package fr.istic.iodeman.strategy;
+
+import java.util.Collection;
+
+import fr.istic.iodeman.model.OralDefense;
+
+public class PlanningExcelExport implements PlanningExport {
+
+	@Override
+	public void execute(Collection<OralDefense> oralDefenses) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
