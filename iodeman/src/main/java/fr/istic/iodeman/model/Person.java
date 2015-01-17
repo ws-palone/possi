a402b770faf1413e01cd8c68b441435fd2610063
paulinelv <paulinelv29@gmail.com>
2015-01-17 12:53:58 +0100

    test création table Person + ajout données dans BDD local

--- iodeman/src/main/java/fr/istic/iodeman/model/Person.java
+++ iodeman/src/main/java/fr/istic/iodeman/model/Person.java
@@ -1,9 +1,24 @@
 package fr.istic.iodeman.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "person")
 public class Person {
+	@Id
+	@GeneratedValue
+	@Column(name="ID")
 	private Integer id;
+	@Column(name="FIRSTNAME")
 	private String firstName;
+	@Column(name="LASTNAME")
 	private String lastName;
+	@Column(name="ROLE")
 	private Role role;
 	
 	public Integer getId() {
@@ -12,12 +27,14 @@ public class Person {
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
 	public String getFirstName() {
 		return firstName;
 	}
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
@@ -30,6 +47,5 @@ public class Person {
 	public void setRole(Role role) {
 		this.role = role;
 	}
-	
 	
 }
---
+++ iodeman/src/test/java/fr/istic/iodeman/dao/TestHibernate.java
@@ -0,0 +1,59 @@
+package fr.istic.iodeman.dao;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.hibernate.Session;
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.istic.iodeman.model.Person;
+import fr.istic.iodeman.model.Role;
+import fr.istic.iodeman.utils.HibernateUtil;
+
+public class TestHibernate {
+
+	/*@Before
+	public void init(){
+
+	}*/
+
+	@Test
+	public void test() {
+	
+		
+		Person per = new Person();
+		per.setFirstName("Pankaj");
+		per.setLastName("blabla");
+		per.setRole(Role.STUDENT);
+		//Get Session
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		//start transaction
+		session.beginTransaction();
+		//Save the Model object
+		System.out.println(per.getId());
+		session.save(per);
+		System.out.println(per.getId());
+		//Commit transaction
+		session.getTransaction().commit();
+		//terminate session factory, otherwise program won't end
+		HibernateUtil.getSessionFactory().close();
+		
+	
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+		EntityManager em = emf.createEntityManager();
+		Person personDB = em.find(Person.class, per.getId());
+		
+		assertTrue(per.getId().equals(personDB.getId()));
+		assertTrue(per.getFirstName().equals(personDB.getFirstName()));
+		assertTrue(per.getLastName().equals(personDB.getLastName()));
+		assertTrue(per.getRole().equals(personDB.getRole()));
+		
+	}
+
+
+}
