3b5a193d5a87cd9175b82b28ef2eacbcb7cfb4ef
Corentin <corentin.clement.pro@gmail.com>
2015-01-20 18:11:02 +0100

    Adding of RestController annotation

--- iodeman/src/main/java/fr/istic/iodeman/controller/PlanningController.java
+++ iodeman/src/main/java/fr/istic/iodeman/controller/PlanningController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import fr.istic.iodeman.model.Planning;
 import fr.istic.iodeman.service.PlanningService;
 
-@RequestMapping("/planning")
+@RequestMapping("/planning") 
+@RestController
 public class PlanningController {
 
 	@Autowired
