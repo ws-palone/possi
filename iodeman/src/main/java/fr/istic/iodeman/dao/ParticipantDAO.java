5cbaa0c9266bdca5eafb70e0b255fe91442bf1b2
genyvra <genyvra@hotmail.fr>
2015-01-20 15:31:43 +0100

    Ajout des méthodes DAO au participants

--- iodeman/src/main/java/fr/istic/iodeman/dao/ParticipantDAO.java
+++ iodeman/src/main/java/fr/istic/iodeman/dao/ParticipantDAO.java
@@ -2,10 +2,41 @@ package fr.istic.iodeman.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import fr.istic.iodeman.model.OralDefense;
 import fr.istic.iodeman.model.Participant;
 
 public interface ParticipantDAO {
 	
 	public List<Participant> findAll();
+	
+	 public void persist(Participant par);
+		
+		public void update(Participant par);
+		
+		public Participant findById(int ID);
+		
+		public void delete(Participant par);
+		
+		public void deleteAll();
+		
+		public Session openCurrentSession();
+
+		public Session openCurrentSessionwithTransaction() ;
+		
+		public void closeCurrentSession() ;
+		
+		public void closeCurrentSessionwithTransaction();
+
+
+		public Session getCurrentSession();
+
+		public void setCurrentSession(Session currentSession);
+		public Transaction getCurrentTransaction();
+
+		public void setCurrentTransaction(Transaction currentTransaction);
+		
 
 }
--- iodeman/src/main/java/fr/istic/iodeman/dao/ParticipantDAOImpl.java
+++ iodeman/src/main/java/fr/istic/iodeman/dao/ParticipantDAOImpl.java
@@ -3,28 +3,106 @@ package fr.istic.iodeman.dao;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 import org.springframework.transaction.annotation.Transactional;
 
+import fr.istic.iodeman.model.OralDefense;
 import fr.istic.iodeman.model.Participant;
 
 public class ParticipantDAOImpl implements ParticipantDAO{
 	
-	 private SessionFactory sessionFactory;
-	 
-	    public ParticipantDAOImpl(SessionFactory sessionFactory) {
-	        this.sessionFactory = sessionFactory;
-	    }
-	 
-	    @Override
-	    @Transactional
-	    public List<Participant> findAll() {
-	        @SuppressWarnings("unchecked")
-	        List<Participant> listParticipant = (List<Participant>) sessionFactory.getCurrentSession()
-	                .createCriteria(Participant.class)
-	                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
-	 
-	        return listParticipant;
-	    }
+	private Session currentSession;
+	private Transaction currentTransaction;
+
+
+	public ParticipantDAOImpl() {
+
+	}
+	public Session openCurrentSession() {
+		currentSession = getSessionFactory().openSession();
+		return currentSession;
+	}
+
+	public Session openCurrentSessionwithTransaction() {
+		currentSession = getSessionFactory().openSession();
+		currentTransaction = currentSession.beginTransaction();
+		return currentSession;
+	}
+
+	public void closeCurrentSession() {
+		currentSession.close();
+	}
+
+	public void closeCurrentSessionwithTransaction() {
+		currentTransaction.commit();
+		currentSession.close();
+	}
+
+	private static SessionFactory getSessionFactory() {
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+		.applySettings(configuration.getProperties());
+		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+		return sessionFactory;
+	}
+
+	public Session getCurrentSession() {
+		return currentSession;
+	}
+
+	public void setCurrentSession(Session currentSession) {
+		this.currentSession = currentSession;
+	}
+
+	public Transaction getCurrentTransaction() {
+		return currentTransaction;
+	}
+
+	public void setCurrentTransaction(Transaction currentTransaction) {
+		this.currentTransaction = currentTransaction;
+	}
+
+	public void persist(Participant par) {
+		getCurrentSession().save(par);
+	}
+
+	public void update(Participant par) {
+		getCurrentSession().update(par);
+	}
+
+	public Participant findById(String id) {
+		Participant par = (Participant) getCurrentSession().get(Participant.class, id);
+		return par; 
+	}
+
+	public void delete(Participant entity) {
+		getCurrentSession().delete(entity);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Participant> findAll() {
+		List<Participant> par = (List<Participant>) getCurrentSession().createQuery("from participant").list();
+		return par;
+	}
+
+	public void deleteAll() {
+		List<Participant> entityList = findAll();
+		for (Participant entity : entityList) {
+			delete(entity);
+		}
+	}
+
+	@Override
+	public Participant findById(int Id) {
+
+		Participant par = (Participant) getCurrentSession().get(Participant.class, Id);
+
+		return par;
+
+    }
 
 }
