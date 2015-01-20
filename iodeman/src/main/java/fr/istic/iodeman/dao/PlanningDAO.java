31790919d36dc2f7b34c1b5abcc3ad1f03de44f6
genyvra <genyvra@hotmail.fr>
2015-01-20 15:35:51 +0100

    Ajout des méthodes DAO au planning

--- iodeman/src/main/java/fr/istic/iodeman/dao/PlanningDAO.java
+++ iodeman/src/main/java/fr/istic/iodeman/dao/PlanningDAO.java
@@ -2,9 +2,41 @@ package fr.istic.iodeman.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import fr.istic.iodeman.model.Participant;
 import fr.istic.iodeman.model.Planning;
 
 public interface PlanningDAO {
 
 	public List<Planning> findAll();
+	
+	 public void persist(Planning pla);
+		
+		public void update(Planning pla);
+		
+		public Planning findById(int ID);
+		
+		public void delete(Planning pla);
+		
+		public void deleteAll();
+		
+		public Session openCurrentSession();
+
+		public Session openCurrentSessionwithTransaction() ;
+		
+		public void closeCurrentSession() ;
+		
+		public void closeCurrentSessionwithTransaction();
+
+
+		public Session getCurrentSession();
+
+		public void setCurrentSession(Session currentSession);
+		public Transaction getCurrentTransaction();
+
+		public void setCurrentTransaction(Transaction currentTransaction);
+		
+
 }
--- iodeman/src/main/java/fr/istic/iodeman/dao/PlanningDAOImpl.java
+++ iodeman/src/main/java/fr/istic/iodeman/dao/PlanningDAOImpl.java
@@ -3,28 +3,106 @@ package fr.istic.iodeman.dao;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 import org.springframework.transaction.annotation.Transactional;
 
+import fr.istic.iodeman.model.Participant;
 import fr.istic.iodeman.model.Planning;
 
 public class PlanningDAOImpl implements PlanningDAO {
 	
-	private SessionFactory sessionFactory;
-	 
-    public PlanningDAOImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
- 
-    @Override
-    @Transactional
-    public List<Planning> findAll() {
-        @SuppressWarnings("unchecked")
-        List<Planning> listPlanning = (List<Planning>) sessionFactory.getCurrentSession()
-                .createCriteria(Planning.class)
-                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
- 
-        return listPlanning;
+	private Session currentSession;
+	private Transaction currentTransaction;
+
+
+	public PlanningDAOImpl() {
+
+	}
+	public Session openCurrentSession() {
+		currentSession = getSessionFactory().openSession();
+		return currentSession;
+	}
+
+	public Session openCurrentSessionwithTransaction() {
+		currentSession = getSessionFactory().openSession();
+		currentTransaction = currentSession.beginTransaction();
+		return currentSession;
+	}
+
+	public void closeCurrentSession() {
+		currentSession.close();
+	}
+
+	public void closeCurrentSessionwithTransaction() {
+		currentTransaction.commit();
+		currentSession.close();
+	}
+
+	private static SessionFactory getSessionFactory() {
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+		.applySettings(configuration.getProperties());
+		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+		return sessionFactory;
+	}
+
+	public Session getCurrentSession() {
+		return currentSession;
+	}
+
+	public void setCurrentSession(Session currentSession) {
+		this.currentSession = currentSession;
+	}
+
+	public Transaction getCurrentTransaction() {
+		return currentTransaction;
+	}
+
+	public void setCurrentTransaction(Transaction currentTransaction) {
+		this.currentTransaction = currentTransaction;
+	}
+
+	public void persist(Planning pla) {
+		getCurrentSession().save(pla);
+	}
+
+	public void update(Planning pla) {
+		getCurrentSession().update(pla);
+	}
+
+	public Planning findById(String id) {
+		Planning pla = (Planning) getCurrentSession().get(Planning.class, id);
+		return pla; 
+	}
+
+	public void delete(Planning entity) {
+		getCurrentSession().delete(entity);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Planning> findAll() {
+		List<Planning> pla = (List<Planning>) getCurrentSession().createQuery("from planning").list();
+		return pla;
+	}
+
+	public void deleteAll() {
+		List<Planning> entityList = findAll();
+		for (Planning entity : entityList) {
+			delete(entity);
+		}
+	}
+
+	@Override
+	public Planning findById(int Id) {
+
+		Planning pla = (Planning) getCurrentSession().get(Planning.class, Id);
+
+		return pla;
+
     }
 
 }
