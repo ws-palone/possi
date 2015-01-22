3952391919b75c63066ef2cdecc528507e0c2348
Corentin <corentin.clement.pro@gmail.com>
2015-01-22 10:53:41 +0100

    Suppression de fonctions inutiles dans l'interface DAO

--- iodeman/src/main/java/fr/istic/iodeman/dao/OralDefenseDAO.java
+++ iodeman/src/main/java/fr/istic/iodeman/dao/OralDefenseDAO.java
@@ -9,34 +9,14 @@ import fr.istic.iodeman.model.OralDefense;
 import fr.istic.iodeman.model.Person;
 
 public interface OralDefenseDAO {
-	
+
 	public List<OralDefense> findAll();
-	
-	 public void persist(OralDefense oral);
-		
-		public void update(OralDefense oral);
-		
-		public OralDefense findById(int ID);
-		
-		public void delete(OralDefense oral);
-		
-		public void deleteAll();
-		
-		public Session openCurrentSession();
 
-		public Session openCurrentSessionwithTransaction() ;
-		
-		public void closeCurrentSession() ;
-		
-		public void closeCurrentSessionwithTransaction();
+	public void persist(OralDefense oral);
 
+	public OralDefense findById(int ID);
 
-		public Session getCurrentSession();
+	public void delete(OralDefense oral);
 
-		public void setCurrentSession(Session currentSession);
-		public Transaction getCurrentTransaction();
-
-		public void setCurrentTransaction(Transaction currentTransaction);
-		
-
+	public void deleteAll();
 }
