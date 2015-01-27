55cbf7e15442e2e8630260ef4fee947aaa2bf378
Kyozatch <lecut.alexandre@gmail.com>
2015-01-27 17:32:44 +0100

    maj WebApplicationConfig

--- iodeman/src/main/java/fr/istic/iodeman/WebApplicationConfig.java
+++ iodeman/src/main/java/fr/istic/iodeman/WebApplicationConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebApplicationConfig extends WebMvcConfigurerAdapter  {
 	
 	public void addInterceptors(InterceptorRegistry registry) {
