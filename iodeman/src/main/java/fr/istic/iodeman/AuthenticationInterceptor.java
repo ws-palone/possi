5cd34b1404bb716aac71beb96758725ff4aaaaf5
Kyozatch <lecut.alexandre@gmail.com>
2015-01-27 17:34:55 +0100

    maj WAC

--- iodeman/src/main/java/fr/istic/iodeman/AuthenticationInterceptor.java
+++ iodeman/src/main/java/fr/istic/iodeman/AuthenticationInterceptor.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import edu.yale.its.tp.cas.client.ServiceTicketValidator;
@@ -32,7 +31,7 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
 	     }
 	     
 	     if(!isValidated && !request.getRequestURI().contains("login")){
-    		 response.sendRedirect("/loginFailed");
+    		 response.sendRedirect("loginFailed");
     		 return false;
     	 }
 	     
--- iodeman/src/main/java/fr/istic/iodeman/WebApplicationConfig.java
+++ iodeman/src/main/java/fr/istic/iodeman/WebApplicationConfig.java
@@ -1,7 +1,6 @@
 package fr.istic.iodeman;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
