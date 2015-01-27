11eff74d0deac3c72b7377cb7a6d3b351cd1b4ad
Kyozatch <lecut.alexandre@gmail.com>
2015-01-27 11:30:18 +0100

    user maj

--- iodeman/src/main/java/fr/istic/iodeman/controller/PersonController.java
+++ iodeman/src/main/java/fr/istic/iodeman/controller/PersonController.java
@@ -3,19 +3,25 @@ package fr.istic.iodeman.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.istic.iodeman.model.Person;
+import fr.istic.iodeman.resolver.PersonResolver;
 
 @RestController
 public class PersonController {
 	
+	@Autowired
+	PersonResolver resolver;
+	
 	@RequestMapping("/user")
-	public String user(HttpSession session){
-
-		return session.getAttribute("uid").toString();
+	public Person user(HttpSession session){
+		
+		String uid = session.getAttribute("uid").toString();
+		return resolver.resolve(uid);
 		
 	}
 
