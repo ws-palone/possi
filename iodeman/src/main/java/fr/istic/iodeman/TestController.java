cb26249aee48bf710eca17624efb1ddbd74e819e
David <contact@davidmichel.fr>
2015-01-29 18:29:55 +0100

    test get request url

--- iodeman/src/main/java/fr/istic/iodeman/TestController.java
+++ iodeman/src/main/java/fr/istic/iodeman/TestController.java
@@ -37,7 +37,7 @@ public class TestController {
 
 	@RequestMapping("/test")
 	public String test(HttpServletRequest request) {
-		return request.getLocalName();
+		return request.getRequestURL().toString();
 	}
 	
 }  
