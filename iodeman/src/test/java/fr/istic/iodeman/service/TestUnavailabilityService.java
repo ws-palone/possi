92f3c507159739b1e5c786748d1eb9359eaca338
Kyozatch <lecut.alexandre@gmail.com>
2015-02-04 15:23:50 +0100

    TestUnavailabilityService /create

--- iodeman/src/test/java/fr/istic/iodeman/service/TestUnavailabilityService.java
+++ iodeman/src/test/java/fr/istic/iodeman/service/TestUnavailabilityService.java
@@ -1,5 +1,7 @@
 package fr.istic.iodeman.service;
 
+import static org.junit.Assert.*;
+
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,14 +18,18 @@ import fr.istic.iodeman.model.Person;
 import fr.istic.iodeman.model.Planning;
 import fr.istic.iodeman.model.TimeBox;
 import fr.istic.iodeman.model.Unavailability;
+import fr.istic.iodeman.resolver.PersonUidResolver;
 
 public class TestUnavailabilityService {
-	/*
+	
 	@InjectMocks
 	private UnavailabilityService unavailabilityService;
 	
 	@Mock
 	private UnavailabilityDAO unavailabilityDAO;
+	
+	@Mock
+	private PersonUidResolver personUidResolver;
 	
 	@Mock
 	private PersonDAO personDAO;
@@ -37,28 +43,33 @@ public class TestUnavailabilityService {
 		MockitoAnnotations.initMocks(this);
 	}
 	
-	//@Test
+	@Test
 	public void testCreate() {
 		
 		Person p1 = new Person();p1.setUid("11008880");
 		Person p2 = new Person();p2.setUid("dCertain");
 		
-		//Participant part = new Participant();part.setFollowingTeacher(p2);part.setStudent(p1);
-		//List<Participant> parts = Lists.newArrayList(part);
+		Mockito.when(personUidResolver.resolve("dCertain")).thenReturn(p2);
 		
-		Planning planning = new Planning();//planning.setAdmin(p2);planning.setParticipants(parts);
+		Planning planning = new Planning();
 		planning.setId(777);
+		
+		Mockito.when(planningDAO.findById(planning.getId())).thenReturn(planning);
 		
 		TimeBox tb = new TimeBox();
 		tb.setFrom(new DateTime(2015,1,10,8,0).toDate());
 		tb.setTo(new DateTime(2015,1,14,8,0).toDate());
 		
-		unavailabilityService.create(planning.getId(), "dCertain", tb);
+		Unavailability unavailability = unavailabilityService.create(planning.getId(), "dCertain", tb);
 		
 		ArgumentCaptor<Unavailability> argumentU = ArgumentCaptor.forClass(Unavailability.class);
 		Mockito.verify(unavailabilityDAO).persist(argumentU.capture());
 		
 		Unavailability u = argumentU.getValue();
 		
-	}*/
+		assertEquals(u.getPerson(), p2);
+		assertEquals(u.getId(), unavailability.getId());
+		assertEquals(u.getPlanning().getId(), unavailability.getPlanning().getId(), 777);
+		
+	}
 }
