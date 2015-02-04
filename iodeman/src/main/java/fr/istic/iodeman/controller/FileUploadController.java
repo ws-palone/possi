bd75b86b059839d129609d1a3666dfc0a695d6d7
Corentin <corentin.clement.pro@gmail.com>
2015-02-04 09:25:17 +0100

    [backend]Modification du file upload - Récupération du planning dynamiquement + redirection

--- iodeman/src/main/java/fr/istic/iodeman/controller/FileUploadController.java
+++ iodeman/src/main/java/fr/istic/iodeman/controller/FileUploadController.java
@@ -6,16 +6,16 @@ import java.io.FileOutputStream;
 
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import fr.istic.iodeman.model.Planning;
 import fr.istic.iodeman.service.PlanningService;
 
-@RestController
+@Controller
 public class FileUploadController {
 
 	@Autowired
@@ -23,10 +23,7 @@ public class FileUploadController {
 	
     @SuppressWarnings("finally")
 	@RequestMapping(value="/upload", method=RequestMethod.POST)
-    public String handleFileUpload(@RequestParam("planningId") int planningId, @RequestParam("file") MultipartFile inputFile){
-    	// the return of the function
-    	String result = "";
-    	
+    public String handleFileUpload(@RequestParam("planningId") int planningId, @RequestParam("file") MultipartFile inputFile, @RequestParam(value="redirectURL", required=false) String redirectURL){    	
     	// path to save the input file
     	String name = "/tmp/"+new DateTime();
     	
@@ -39,29 +36,28 @@ public class FileUploadController {
                 BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(outputFile));
                 stream.write(bytes);
                 stream.close();
-                result =  "Participants ajoutés" + name + "!";
+//                result =  "Participants ajoutés" + name + "!";
             } catch (Exception e) {
-            	result =  "Erreur lors du téléchargement du fichier " + name + " => " + e.getMessage();
+//            	result =  "Erreur lors du téléchargement du fichier " + name + " => " + e.getMessage();
+            	e.printStackTrace();
             }
         } else {
-        	result =  "Erreur car votre fichier " + name + " est vide";
+//        	result =  "Erreur car votre fichier " + name + " est vide";
         }
     	
     	// on l'envoie au service
-//    	Planning planning = planningService.findById(new Integer(planningId));
-    	Planning planning = new Planning();
-    	planning.setName("M2MIAGE 2015");
+    	Planning planning = planningService.findById(new Integer(planningId));
     	try {
 			Planning filledPlanning = planningService.importPartcipants(planning, outputFile);
 		} catch (Exception e) {
-			result =  "Erreur lors de l'import des participants : ";
+//			result =  "Erreur lors de l'import des participants : ";
 			e.printStackTrace();
 			// affcher erreur de format
 		} finally {
 	    	// on supprime le fichier
 	    	outputFile.delete();
 	    	
-	    	return result;
+	    	return "redirect:"+redirectURL;
 		}
 
     }
