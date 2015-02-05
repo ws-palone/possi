ca0834e192fc03f1d85fdbd3362f1b2a7540e5c2
Corentin <corentin.clement.pro@gmail.com>
2015-02-05 11:10:31 +0100

    [backend] ajout de exportAgenda dans le service Unavailability

--- iodeman/src/main/java/fr/istic/iodeman/service/UnavailabilityService.java
+++ iodeman/src/main/java/fr/istic/iodeman/service/UnavailabilityService.java
@@ -1,8 +1,11 @@
 package fr.istic.iodeman.service;
 
-import java.util.Date;
+import java.util.Collection;
 import java.util.List;
 
+import fr.istic.iodeman.dto.AgendaDTO;
+import fr.istic.iodeman.model.Person;
+import fr.istic.iodeman.model.Planning;
 import fr.istic.iodeman.model.TimeBox;
 import fr.istic.iodeman.model.Unavailability;
 
@@ -13,5 +16,7 @@ public interface UnavailabilityService {
 	public Unavailability create(Integer id, String uidperson, TimeBox period);
 
 	public Unavailability delete(Integer id);
+	
+	public Collection<AgendaDTO> exportAgenda(Planning planning, Person person);
 
 }
