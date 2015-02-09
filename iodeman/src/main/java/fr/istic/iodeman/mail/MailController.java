a92f00d848ba98135360d8f1dd6c6bdae7451e5a
Kyozatch <lecut.alexandre@gmail.com>
2015-02-09 16:51:17 +0100

    commit pour battre david

--- iodeman/src/main/java/fr/istic/iodeman/mail/MailController.java
+++ iodeman/src/main/java/fr/istic/iodeman/mail/MailController.java
@@ -1,9 +1,9 @@
 package fr.istic.iodeman.mail;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MailController {
 	
 	private String MAIL_SERVER;
