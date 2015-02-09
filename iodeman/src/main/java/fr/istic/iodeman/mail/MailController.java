98b97b82a4347ea3ab27a3128e64222b618bd68f
Kyozatch <lecut.alexandre@gmail.com>
2015-02-09 16:05:15 +0100

    mail test

---
+++ iodeman/src/main/java/fr/istic/iodeman/mail/MailController.java
@@ -0,0 +1,24 @@
+package fr.istic.iodeman.mail;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MailController {
+	
+	private String MAIL_SERVER;
+	private String MAIL_TYPE;
+	private String MAIL_TO;
+	
+	@RequestMapping("/mail")
+	public String redirectToNewMail(){
+		
+		MAIL_SERVER = "https://webmail.etudiant.univ-rennes1.fr/dimp/compose.php";
+		MAIL_TYPE = "new";
+		MAIL_TO = "lecut.alexandre@gmail.com";
+		
+		return MAIL_SERVER+"?type"+MAIL_TYPE+"&to="+MAIL_TO;
+		
+	}
+
+}
