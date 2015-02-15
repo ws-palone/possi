c2d3671766d318d767fa9af8898e1f0a33adbd18
David <contact@davidmichel.fr>
2015-02-15 17:58:48 +0100

    [backend] add unit test for method delete of UnavailabilityService

--- iodeman/src/test/java/fr/istic/iodeman/service/TestUnavailabilityService.java
+++ iodeman/src/test/java/fr/istic/iodeman/service/TestUnavailabilityService.java
@@ -2,6 +2,8 @@ package fr.istic.iodeman.service;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +12,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import com.google.common.collect.Lists;
 
 import fr.istic.iodeman.dao.PersonDAO;
 import fr.istic.iodeman.dao.PlanningDAO;
@@ -70,6 +74,54 @@ public class TestUnavailabilityService {
 		assertEquals(u.getPerson(), p2);
 		assertEquals(u.getId(), unavailability.getId());
 		assertEquals(u.getPlanning().getId(), unavailability.getPlanning().getId(), 777);
+		
+	}
+	
+	@Test
+	public void testDelete() {
+		
+		Person p2 = new Person();p2.setUid("dCertain");
+		
+		Planning planning = new Planning();
+		planning.setAdmin(p2);
+		planning.setId(777);
+		
+		Mockito.when(planningDAO.findById(planning.getId())).thenReturn(planning);
+		
+		TimeBox tb = new TimeBox();
+		tb.setFrom(new DateTime(2015,1,13,9,0).toDate());
+		tb.setTo(new DateTime(2015,1,13,11,0).toDate());
+		
+		List<Unavailability> unavailabilities = Lists.newArrayList();
+		
+		Unavailability ua1 = new Unavailability();
+		ua1.setPeriod(new TimeBox(
+				new DateTime(2015,1,13,8,45).toDate(),
+				new DateTime(2015,1,13,9,15).toDate()
+		));
+		unavailabilities.add(ua1);
+		
+		Unavailability ua2 = new Unavailability();
+		ua2.setPeriod(new TimeBox(
+				new DateTime(2015,1,13,10,0).toDate(),
+				new DateTime(2015,1,13,11,0).toDate()
+		));
+		unavailabilities.add(ua2);
+		
+		Unavailability ua3 = new Unavailability();
+		ua3.setPeriod(new TimeBox(
+				new DateTime(2015,1,13,14,45).toDate(),
+				new DateTime(2015,1,13,15,15).toDate()
+		));
+		unavailabilities.add(ua3);
+		
+		Mockito.when(unavailabilityDAO.findById(planning.getId(), p2.getUid())).thenReturn(unavailabilities);
+		
+		unavailabilityService.delete(planning.getId(), p2.getUid(), tb);
+		
+		Mockito.verify(unavailabilityDAO, Mockito.times(1)).delete(ua1);
+		Mockito.verify(unavailabilityDAO, Mockito.times(1)).delete(ua2);
+		Mockito.verify(unavailabilityDAO, Mockito.times(0)).delete(ua3);
 		
 	}
 }
