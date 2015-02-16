8b75653817b3b70f094a77cc35a7a936f21c643f
Kyozatch <lecut.alexandre@gmail.com>
2015-02-16 16:16:15 +0100

    modif distinct mail for student and teacher

--- iodeman/src/main/java/fr/istic/iodeman/service/MailServiceImpl.java
+++ iodeman/src/main/java/fr/istic/iodeman/service/MailServiceImpl.java
@@ -3,10 +3,16 @@ package fr.istic.iodeman.service;
 import java.util.Collection;
 
 import org.apache.commons.lang.Validate;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Lists;
+
 import fr.istic.iodeman.dao.PlanningDAO;
+import fr.istic.iodeman.model.OralDefense;
 import fr.istic.iodeman.model.Participant;
 import fr.istic.iodeman.model.Person;
 import fr.istic.iodeman.model.Planning;
@@ -22,6 +28,7 @@ public class MailServiceImpl implements MailService{
 	private String MAIL_TO;
 	private String MAIL_OBJECT;
 
+	@SuppressWarnings("null")
 	@Override
 	public String sendToEveryParticipant(Integer idPlanning) {
 		
@@ -39,13 +46,33 @@ public class MailServiceImpl implements MailService{
 		Collection<Participant> participants = null;
 		participants = planningDAO.findParticipants(planning);
 		
+		Collection<String> participantMailStudent = null;
+		Collection<String> participantMailTeacher = null;
+
 		Validate.notNull(participants);
 		
 		for(Participant p : participants){
-			MAIL_TO+= p.getStudent().getEmail()+",";
+			participantMailStudent.add(p.getStudent().getEmail());
+			participantMailTeacher.add(p.getFollowingTeacher().getEmail());
+		}
+		
+		participantMailStudent = getSingleMailFromMailList(participantMailStudent);
+		participantMailTeacher = getSingleMailFromMailList(participantMailTeacher);
+		
+		for(String p : participantMailStudent){
+			MAIL_TO+= p+",";
+		}
+		
+		for(String p : participantMailTeacher){
+			MAIL_TO+= p+",";
 		}
 		
 		return "redirect:"+MAIL_SERVER+"?type="+MAIL_TYPE+"&subject="+MAIL_OBJECT+"&to="+MAIL_TO;
+	}
+	
+	private Collection<String> getSingleMailFromMailList(Collection<String> participantMail){
+
+		return Lists.newArrayList(ImmutableSortedSet.copyOf(participantMail));
 	}
 	
 	
