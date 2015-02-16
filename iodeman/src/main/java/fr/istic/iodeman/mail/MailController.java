c55a8c50e5ce8369ba38ced3d966d575c65591eb
Kyozatch <lecut.alexandre@gmail.com>
2015-02-16 15:39:30 +0100

    modif restMail

--- iodeman/src/main/java/fr/istic/iodeman/mail/MailController.java
+++ iodeman/src/main/java/fr/istic/iodeman/mail/MailController.java
@@ -1,13 +1,13 @@
 package fr.istic.iodeman.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import fr.istic.iodeman.service.MailService;
 
-@Controller
+@RestController
 public class MailController {
 	
 	@Autowired
