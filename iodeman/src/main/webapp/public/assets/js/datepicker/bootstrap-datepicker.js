28949ec1da9d76ba21aab60d830dd5e14ffbcc53
Corentin <corentin.clement.pro@gmail.com>
2015-02-19 18:42:08 +0100

    [frontend] translate datepicker in french

--- iodeman/src/main/webapp/public/assets/js/datepicker/bootstrap-datepicker.js
+++ iodeman/src/main/webapp/public/assets/js/datepicker/bootstrap-datepicker.js
@@ -369,11 +369,15 @@
 				navStep: 10
 		}],
 		dates:{
-			days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-			daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-			daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-			months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-			monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+	        days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
+	        daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"],
+	        daysMin: ["D", "L", "Ma", "Me", "J", "V", "S", "D"],
+	        months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+	        monthsShort: ["Jan", "Fév", "Mar", "Avr", "Mai", "Jui", "Jul", "Aou", "Sep", "Oct", "Nov", "Déc"],
+	        today: "Aujourd'hui",
+	        clear: "Effacer",
+	        weekStart: 1,
+	        format: "dd/mm/yyyy"
 		},
 		isLeapYear: function (year) {
 			return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0))
