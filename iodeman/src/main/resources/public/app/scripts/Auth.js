2c2eaf7329f726ba1568122c932fa565ef3edbbd
François Esnault <francois.esnault53@gmail.com>
2016-02-22 13:08:05 +0100

    no message

--- iodeman/src/main/resources/public/app/scripts/Auth.js
+++ iodeman/src/main/resources/public/app/scripts/Auth.js
@@ -1,14 +1,9 @@
 angular.module('publicApp')
 
-.factory('Auth', function($sessionStorage, $location, backend, $http, $window){
+.factory('Auth', function($sessionStorage, $location, backendURL, $http, $window){
 	return{
 		login : function(){
-			userRequest = backend.getUser();
-			userRequest.success(function(data) {
-				$sessionStorage.user = data;
-				return data;
-			});
-			userRequest.error(function(data) {
+			$http.get(backendURL + 'user').error(function(data) {
 				$window.location.href = $location.protocol() + '://' + $location.host() + ':' +  $location.port() + '/api/auth/login';
 			});
 		}
