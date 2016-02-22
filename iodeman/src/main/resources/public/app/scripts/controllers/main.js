bd35b1a0edea68d9b1c017243feb2811f762aefc
hvallee <hugo.v.vallee@gmail.com>
2016-02-22 11:21:46 +0100

    connection screen

--- iodeman/src/main/resources/public/app/scripts/controllers/main.js
+++ iodeman/src/main/resources/public/app/scripts/controllers/main.js
@@ -10,18 +10,23 @@
 angular.module('publicApp')
 .controller('MainCtrl', function ($scope, backend, Auth, $sessionStorage, $rootScope, $timeout) {
 	$.material.init();
+	$scope.connected = false;
 	$scope.user = {};
 	$scope.plannings = new Array();
 	
 	$scope.user = $sessionStorage.user;
 	
 	if($scope.user == null) {
+
 		backend.getUser().then(function (success) {
 			$scope.user = success.data;
 			$scope.$digest;
 		});
 	}
-		
+	if($sessionStorage.user != null) {
+		$scope.connected = true;
+	}
+
 	backend.plannings.list().then(function(data) {
 		$scope.plannings = data;
 		$scope.$digest();
