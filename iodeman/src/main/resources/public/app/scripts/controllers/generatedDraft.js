e5ace9de2bb325ef3dc642f2c9d9bf9cf2fc98b4
dania <dania.kaakati@gmail.com>
2017-01-27 16:11:37 +0100

    Enregistrer dans le cache detection des modifications comparaison Pour le retrofit

--- iodeman/src/main/resources/public/app/scripts/controllers/generatedDraft.js
+++ iodeman/src/main/resources/public/app/scripts/controllers/generatedDraft.js
@@ -1,70 +1,123 @@
 angular.module('publicApp')
-	.controller('GeneratedDraftCtrl', function ($scope, $window, $http, backendURL, Auth, $routeParams) {
+    .controller('GeneratedDraftCtrl', function ($scope, $window, $http, backendURL, Auth, $routeParams) {
 
-		$scope.id = $routeParams.idPlanning;
+        $scope.id = $routeParams.idPlanning;
 
-		$scope.printIt = function(){
-			window.print();
-		};
+        $scope.id_div = -1;
 
-		$http.get(backendURL + 'planning/' + $scope.id + '/exportDraft')
-			.success(function(data) {
-				console.log(data.creneaux);
+        $scope.cache = {};
+        $scope.modified = {};
 
-				const ordered = {};
-				Object.keys(data.creneaux).sort().forEach(function(key) {
-					ordered[key] = data.creneaux[key];
-				});
+        $scope.printIt = function () {
+            window.print();
+        };
 
-				data.creneaux = ordered;
-				$scope.creneaux = data;
-				console.log($scope.creneaux);
-				$scope.i = 0;
-			})
-			.error(function(data) {
-				$scope.error = true;
-			});
+        $http.get(backendURL + 'planning/' + $scope.id + '/exportDraft')
+            .success(function (data) {
 
-		function sortByKey(array, key) {
-			return array.sort(function(a, b) {
-				var x = a[key]; var y = b[key];
-				return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-			});
-		}
+                const ordered = {};
+                Object.keys(data.creneaux).sort().forEach(function (key) {
+                    ordered[key] = data.creneaux[key];
+                });
 
-		/*$scope.drag = function (){
+                data.creneaux = ordered;
+                $scope.creneaux = data;
+                $scope.i = 0;
+            })
+            .error(function (data) {
+                $scope.error = true;
+            });
 
-		 }*/
+        function sortByKey(array, key) {
+            return array.sort(function (a, b) {
+                var x = a[key];
+                var y = b[key];
+                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+            });
+        }
+
+        $scope.getId_div = function () {
+            $scope.id_div = $scope.id_div + 1;
+            return $scope.id_div;
+        };
+        $scope.drag = function () {
+            $('.event').on("dragstart", function (event) {
+                var dt = event.originalEvent.dataTransfer;
+                dt.setData('Text', $(this).attr('id'));
+            });
+            $('table td').on("dragenter dragover drop", function (event) {
+                event.preventDefault();
+                if (event.type === 'drop') {
+
+                    var id_drag = $(this).attr('id');
+                    var data = event.originalEvent.dataTransfer.getData('Text', id_drag);
+                    if ($(this).find('span').length === 0) {
+                        de = $('#' + data).detach();
+                        de.appendTo($(this));
+                    }
+                    $scope.modified[data] = {
+                        "room": event.target.cellIndex,
+                        "periode": $('#' + data).parent().parent()[0].firstElementChild.getAttribute('data-periode')
+                    };
+
+                }
+            });
+        };
+
+        $scope.save = function () {
+            $scope.toSend = [];
+
+            angular.forEach($scope.modified, function (value, index) {
+
+                if ($scope.cache[index].salle != value.room || $scope.cache[index].periode != value.periode) {
+
+                    $scope.cache[index].salle = value.room;
+                    $scope.cache[index].periode = value.periode;
+                    $scope.toSend.push($scope.cache[index]);
+                }
+
+            });
+        }
+
+    })
+    .directive('myRepeatDirective', function ($filter) {
+        return function (scope, element, attrs) {
+
+            var capit = $filter('capitalize');
+            var etn = $filter('emailToName');
+            var room_id = element.parent().children('td').length - 1;
+            if (typeof scope.horaire[room_id - 1] !== 'undefined') {
+                var current_id = scope.getId_div();
+
+                var html = "";
+
+                html += '<div class="event creneau" draggable="true" id="' + current_id + '">';
+
+                html += '<table width="100%" class="rect_planning">'
+                    + '<tr>'
+                    + '<td class="rec_etud" width="20%">'
+                    + capit(etn(scope.horaire[room_id - 1].student.name))
+                    + '</td>'
+                    + '<td  class="rec_tut" width="20%">'
+                    + capit(scope.horaire[room_id - 1].student.tuteur.name)
+                    + '</td>'
+                    + '<td  class="rec_prof1" width="20%">'
+                    + capit(etn(scope.horaire[room_id - 1].student.enseignant.name))
+                    + '</td>'
+                    + '<td  class="rec_prof2" width="20%">'
+                    + capit(etn(scope.horaire[room_id - 1].candide.name))
+                    + '</td>';
+
+                html += '</tr></table></div>';
+
+                $(element).append(html);
+                scope.cache[current_id] = scope.horaire[room_id - 1];
+            }
 
 
-	})
-	.directive('myRepeatDirective', function() {
-		return function(scope, element, attrs) {
-			//angular.element(element).css('color','blue');
-			if (scope.$last){
-				$('.event').on("dragstart", function (event) {
-					var dt = event.originalEvent.dataTransfer;
-					dt.setData('Text', $(this).attr('id'));
-				});
-				$('table td').on("dragenter dragover drop", function (event) {
-					event.preventDefault();
-					if (event.type === 'drop') {
-						var data = event.originalEvent.dataTransfer.getData('Text',$(this).attr('id'));
-						if($(this).find('span').length===0){
-							de=$('#'+data).detach();
-							de.appendTo($(this));
-						}
-
-					}
-				});
-
-				var id=0;
-				$(document).find('.event').each(function(){
-					$(this).attr('id',id);
-					++id;
-					console.log($(this).attr('id'));
-				});
-			}
-		};
-	});
+            if (scope.$last) {
+                scope.drag();
+            }
+        };
+    });
 
