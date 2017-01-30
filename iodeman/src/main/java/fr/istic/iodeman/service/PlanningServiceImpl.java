c3adc10877a19d98b8674ffb8976f0b096791732
paulinef <flambard.pauline@laposte.net>
2017-01-30 15:05:19 +0100

    delete draft before ref

--- iodeman/src/main/java/fr/istic/iodeman/service/PlanningServiceImpl.java
+++ iodeman/src/main/java/fr/istic/iodeman/service/PlanningServiceImpl.java
@@ -417,10 +417,11 @@ public class PlanningServiceImpl implements PlanningService {
 	@Override
 	public void delete(Planning planning) {
 
-		planningDAO.delete(planning);
 		planningDAO.deleteDraft(planning.getId());
+		planningDAO.delete(planning);
+
 	}
 
 
-	
+
 }
