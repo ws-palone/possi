4ebd89f4a19a6b5db8cd60823f4450e2a7ff98cd
Koitrin KOFFI <koitrinstephanedesire@gmail.com>
2020-05-08 17:17:47 +0200

    prototype final Fix import csv

--- iodeman/src/main/java/fr/istic/iodeman/strategy/ParticipantsCSVImport.java
+++ iodeman/src/main/java/fr/istic/iodeman/strategy/ParticipantsCSVImport.java
@@ -24,95 +24,100 @@ public class ParticipantsCSVImport implements ParticipantsImport {
 	public void configure(PersonResolver personResolver) {
 		this.personResolver = personResolver;
 	}
-	
+
 	public Collection<OralDefense> execute(File file) throws Exception {
-		
+
 		Collection<OralDefense> participants = new ArrayList<>();
 		Collection<String> students = new ArrayList<>();
-		
+
 		BufferedReader br = null;
-        String line = "";
-        String cvsSplitBy = ";";
-        int lineNumber = 0;
-        
-        System.err.println("Start Import");
-        
-        try {
-            br = new BufferedReader(new FileReader(file));
-            line = br.readLine();
-            if(!line.contains(",")) {
-            	cvsSplitBy = ";";
-            }
-            if(!line.contains(";")) {
-            	cvsSplitBy = ",";
-            }
-            while ((line = br.readLine()) != null) {
-            	lineNumber ++;
-            	System.err.println(line);
-                // use comma as separator
-                String[] row = line.split(cvsSplitBy);
-                System.err.println(row[1].trim() + " " + row[2].trim());
-                String stu_nom = row[1].trim();
-                String stu_prenom = row[2].trim();
-                
-                // create participants
-    			OralDefense participant = new OralDefense();
-				String emailStudent = row[0].trim();
-				String emailTeacher = row[4].trim();
-				String normedEmailStudent = normalize(emailStudent);
-				Person student = personResolver.resolve(normedEmailStudent);
-				String normedEmailTeacher = normalize(emailTeacher);
-				Person followingTeacher = personResolver.resolve(normedEmailTeacher);
+		String line = "";
+		String cvsSplitBy = ";";
+		int lineNumber = 0;
 
-				String tutorFullName = row[5].trim();
-				participant.setTutorFullName(tutorFullName);
-				// Compagny
-				String company = row[3].trim();
-				participant.setCompany(company);
-				participant.setStudent(student);
-				participant.setFollowingTeacher(followingTeacher);
+		System.err.println("Start Import");
 
-				if (emailStudent.equals(emailTeacher)){
-					errorsDisplayer("L'étudiant et le professeur référent ont le même mail. (doublon)", lineNumber);
+		try {
+			br = new BufferedReader(new FileReader(file));
+			line = br.readLine();
+			if(!line.contains(",")) {
+				cvsSplitBy = ";";
+			}
+			if(!line.contains(";")) {
+				cvsSplitBy = ",";
+			}
+			while ((line = br.readLine()) != null) {
+				lineNumber ++;
+				System.err.println(line);
+				// use comma as separator
+				String[] row = line.split(cvsSplitBy);
+				if (row.length == 6) {
+
+					System.err.println(row[1].trim() + " " + row[2].trim());
+					String stu_nom = row[1].trim();
+					String stu_prenom = row[2].trim();
+
+					// create participants
+					OralDefense participant = new OralDefense();
+					String emailStudent = row[0].trim();
+					String emailTeacher = row[4].trim();
+					String normedEmailStudent = normalize(emailStudent);
+					Person student = personResolver.resolve(normedEmailStudent);
+					String normedEmailTeacher = normalize(emailTeacher);
+					Person followingTeacher = personResolver.resolve(normedEmailTeacher);
+
+					String tutorFullName = row[5].trim();
+					participant.setTutorFullName(tutorFullName);
+					// Compagny
+					String company = row[3].trim();
+					participant.setCompany(company);
+					participant.setStudent(student);
+					participant.setFollowingTeacher(followingTeacher);
+
+					if (emailStudent.equals(emailTeacher)){
+						errorsDisplayer("L'étudiant et le professeur référent ont le même mail. (doublon)", lineNumber);
+					}
+					else if (student == null) {
+						errorsDisplayer("La personne renseigné dans le champ étudiant n'est pas un étudiant reconnu.", lineNumber);
+					}
+					else if (student.getRole() != Role.STUDENT){
+						errorsDisplayer("La personne renseigné dans le champ étudiant n'est pas un étudiant reconnu.", lineNumber);
+					}
+					else if (followingTeacher == null) {
+						errorsDisplayer("La personne renseigné dans le champ professeur référent n'est pas un professeur reconnu", lineNumber);
+					}
+					else if (followingTeacher.getRole() == Role.STUDENT){
+						errorsDisplayer("La personne renseigné dans le champ professeur référent n'est pas un professeur reconnu", lineNumber);
+					}
+					else if (students.contains(student.getUid())){
+						errorsDisplayer("L'étudiant est déjà présent dans cet import.", lineNumber);
+					}
+					else if (participants.contains(participant)) {
+						errorsDisplayer("La soutenance existe déjà dans cet import.", lineNumber);
+					}
+					else {
+						students.add(student.getUid());
+						participants.add(participant);
+					}
+				} else {
+					errorsDisplayer("Ligne mal renseigée", lineNumber);
 				}
-				else if (student == null) {
-					errorsDisplayer("La personne renseigné dans le champ étudiant n'est pas un étudiant reconnu.", lineNumber);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-				else if (student.getRole() != Role.STUDENT){
-					errorsDisplayer("La personne renseigné dans le champ étudiant n'est pas un étudiant reconnu.", lineNumber);
-				}
-				else if (followingTeacher == null) {
-					errorsDisplayer("La personne renseigné dans le champ professeur référent n'est pas un professeur reconnu", lineNumber);
-				}
-				else if (followingTeacher.getRole() == Role.STUDENT){
-					errorsDisplayer("La personne renseigné dans le champ professeur référent n'est pas un professeur reconnu", lineNumber);
-				}
-				else if (students.contains(student.getUid())){
-					errorsDisplayer("L'étudiant est déjà présent dans cet import.", lineNumber);
-				}
-				else if (participants.contains(participant)) {
-					errorsDisplayer("La soutenance existe déjà dans cet import.", lineNumber);
-				}
-				else {
-					students.add(student.getUid());
-					participants.add(participant);
-				}
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        System.err.println("end import");
-        return participants;
+			}
+		}
+		System.err.println("end import");
+		return participants;
 	}
-	
+
 	private String normalize(String input){
 		return Normalizer.normalize(input, Normalizer.Form.NFC).replaceAll("[^\\p{ASCII}]", "");
 	}
