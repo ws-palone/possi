d01e96e6a75d4ae28beffcbfa20bf958a37ceadf
Koitrin KOFFI <koitrinstephanedesire@gmail.com>
2020-05-09 15:31:44 +0200

    prototype final fix security

--- iodeman/src/main/java/fr/istic/iodeman/config/SecurityConfig.java
+++ iodeman/src/main/java/fr/istic/iodeman/config/SecurityConfig.java
@@ -34,11 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll().
+        http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().
                 anyRequest().authenticated().and().
                 exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
